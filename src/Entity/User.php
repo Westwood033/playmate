50b6a1306ac913797a87dd11c516606c5bd599a5
Samuel Espallier <s.espallier@sciencespobordeaux.fr>
2026-05-11 13:42:47 +0200

    dashboard

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -27,11 +27,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 180)]
     private ?string $username = null;
 
+    #[ORM\Column(length: 180)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $lastname = null;
+
     /**
      * @var list<string> The user roles
      */
     #[ORM\Column]
-    private array $roles = [];
+    private array $roles;
 
     /**
      * @var string The hashed password
@@ -109,6 +115,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
+    public function addRole(string $role): static
+    {
+        $this->roles[] = $role;
+
+        return $this;
+    }
+
+    public function hasRole(string $role): bool
+    {
+        return in_array($role, $this->roles);
+    }
+
     /**
      * @see PasswordAuthenticatedUserInterface
      */
@@ -150,5 +168,22 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getUpdatedAt(): ?DateTimeImmutable
     {
         return $this->updatedAt;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
     }
 }
