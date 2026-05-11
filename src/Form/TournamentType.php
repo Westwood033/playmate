266fcfca87b816f84a27b149c6bfb6de755d25dc
Westwood <b.loupiac03@gmail.com>
2026-05-11 13:58:28 +0200

    crud tournament feature

---
+++ src/Form/TournamentType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tournament;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TournamentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('creationDate')
+            ->add('tournamentDate')
+            ->add('participantNumber')
+            ->add('address')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tournament::class,
+        ]);
+    }
+}
