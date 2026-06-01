39c3c56ca834f374af1ce24cdf97101f5ab32b0c
YonisDF <yonis.duffau@hotmail.com>
2026-06-01 10:49:36 +0200

    User php fix after merge

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -327,7 +327,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
-}
+    
     public function __toString(): string
     {
         return $this->username;
