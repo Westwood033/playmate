4984f8a303cfaec897687741f6d8ddb6fbf8c73d
Samuel Espallier <s.espallier@sciencespobordeaux.fr>
2026-06-01 11:06:26 +0200

    add item crud controller and fields

--- src/Controller/Admin/ItemCrudController.php
+++ src/Controller/Admin/ItemCrudController.php
@@ -6,7 +6,6 @@ use App\Entity\Item;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
